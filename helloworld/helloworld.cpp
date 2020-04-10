70a44bc410226fbc6164e59c5185e4f1e9df1439
Per Thomas <you@example.com>
2020-04-10 12:41:44 +0200

    debugging

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -26,13 +26,50 @@ using std::cout;
 
 #include <memory>
 
+
+enum class e
+{
+   ONE = 1,
+   TWO = 10,
+   TREE = 1,
+   FOUR = 2
+};
+
+
 class A
 {
 
 };
 
+
 int main()
 {
+   cout << "ONE = "  << (int)e::ONE << endl;
+   cout << "TWO = "  << (int)e::TWO << endl;
+   cout << "TREE = " << (int)e::TREE << endl;
+   cout << "FOUR = " << (int)e::FOUR << endl;
+
+   auto test = e::ONE;
+
+//    switch (  test )
+//    {
+//    case e::ONE:
+//       cout << "ONE" << endl;
+//       break;
+//    case e::TWO:
+//       cout << "TWO" << endl;
+//       break;
+//      case e::TREE:
+//       cout << "TWO" << endl;
+//       break;
+//   case e::FOUR:
+//       cout << "TWO" << endl;
+//       break;
+//    default:
+//       break;
+//    }
+
+   return 0;
    // string s_cwd(getcwd(NULL,0));
    // cout << "CWD is: " << s_cwd << endl;
   //  cout << "ssdir ="  <<  string(g_system()->GetExeDir()) << endl;;
