3b4ee1645594bd47dcd1180c11bc4ed0443cd32f
perthi <pth@embc.no>
2020-04-11 16:08:52 +0200

    effective c++

--- helloworld/helloworld.mk
+++ helloworld/helloworld.mk
@@ -1,22 +1,11 @@
 
 PROGRAM:=helloworld
 SRCCPP+=../helloworld.cpp
-		
-		
-#		../GVersion.cpp
-
-
 
 
 LIBS+=   -lutilities 
-
 LIBS+=$(PROTOBUF_LIBS)
 
-
-INCLUDES+=$(PROTOBUF_INCLUDES)
-
-CPPFLAGS=$(CPPFLAGS_RELAXED)
-CPPFLAGS=$(CPPFLAGS_RELAXED)
 
 
 include ../../common.mk
