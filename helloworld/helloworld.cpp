efcb08c0e49a245448103d955d2f9408dec6c776
perthi <pth@embc.no>
2020-07-05 21:38:06 +0200

    debugging lamda functions

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -13,26 +13,28 @@ using std::string;
 using std::endl;
 using std::cout;
 
-#include <memory>
-#include <iostream> 
-#include <string>
-#include <logging/LLogApi.h>
-#include <logging/LDoc.h>
-#include <logging/LLogTest.h>
-#include <exception/GException.h>
-#include <exception>
+#include   <memory>
+#include   <iostream> 
+#include   <string>
+#include   <logging/LLogApi.h>
+#include   <logging/LDoc.h>
+#include   <logging/LLogTest.h>
+#include   <exception/GException.h>
+#include   <exception>
 using namespace LOGMASTER;
-#include <logging/LPublisher.h>
-#include <queue>
-#include <chrono>
-#include <thread>
-#include <algorithm>
-#include <ostream>
-#include <iostream>
-#include <fstream>
-#include <vector>
+#include   <logging/LPublisher.h>
+#include   <queue>
+#include   <chrono>
+#include   <thread>
+#include   <algorithm>
+#include   <ostream>
+#include   <iostream>
+#include   <fstream>
+#include   <vector>
+
 
 using std::vector;
+
 
 template< typename T >
 class Less
@@ -59,14 +61,22 @@ int count (  const int lim,const C& c, P pred )
 }
 
 
-void print_modulo( const vector<int> & v, std::ostream & os, int m  )
+// template<typename T>
+// void print_modulo( const vector<T> & v, std::ostream & os, int m  )
+// {
+//   for_each( begin(v), end(v),  [&os, m ](auto x) 
+//   {
+//   // if(x%m == 0) os << x << '\n'; 
+//     os << x << '\n'; 
+//   } );
+// }
+
+
+template<typename T>
+void print_modulo( const vector<T> & v, std::ostream & os, int m  )
 {
-
-  for_each( begin(v), end(v),  [&os, m ](int x) 
-  {
-   if(x%m == 0) os << x << '\n'; 
-
-  } );
+  auto print =  [&os, m]( T x) {   os << x << " xxxx " << '\n';  };
+  for_each( begin(v), end(v), print ); 
 
 }
 
@@ -75,15 +85,20 @@ void print_modulo( const vector<int> & v, std::ostream & os, int m  )
 int main()
 {
   Less<int> lti;
-  vector<int> test = {1,2,33,44,55,66,77};
-  int cnt =  count( 42, test, lti );
+
+  vector<int> test1 = {1,2,33,44,55,66,77};
+  vector<double> test2 = {1.1,2.23,33.33,44.44,55.55,66.66,77.77 };
+  vector<string> test3  {"ole", "dole", "doff", "kinkeliane", "koff" };
+
+  int cnt =  count( 42, test1, lti );
   FORCE_DEBUG("The number of matches was %d", cnt );
   std::stringstream out;
-  print_modulo( test, out , 11);
+
+  print_modulo( test1, out , 11);
+  print_modulo( test2, out , 11);
+  print_modulo( test3, out , 11);
+
   CERR << out.str()  << endl;
-
-  
-
 
   // char buf[255];
   // for(int i=0; i < 1000; i++)
@@ -92,5 +107,6 @@ int main()
   //   FORCE_DEBUG("%s", buf);
   // }
   ///std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
 }
 
