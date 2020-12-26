55c6ee0c7cb3dd74f313f8d26bd869483b7cd984
Per Thomas <pth@embc.no>
2020-12-26 18:04:13 +0100

    added missing ncurse and readline libraries

---
+++ submodules/productivity/3rd-party/arm/ncurses-6.2/progs/transform.h
@@ -0,0 +1,10 @@
+#ifndef __TRANSFORM_H
+#define __TRANSFORM_H 1
+#include <progs.priv.h>
+extern bool same_program(const char *, const char *);
+#define PROG_CAPTOINFO "arm-linux-captoinfo"
+#define PROG_INFOTOCAP "arm-linux-infotocap"
+#define PROG_CLEAR     "arm-linux-clear"
+#define PROG_RESET     "arm-linux-reset"
+#define PROG_INIT      "arm-linux-init"
+#endif /* __TRANSFORM_H */
