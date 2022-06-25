d8cfceb1d23610259e57a34df5b4c365fd3ff362
perthi <pth@embc.no>
2022-06-25 23:56:03 +0200

    build sysstem

--- gui/common/gui-common.mk
+++ gui/common/gui-common.mk
@@ -4,22 +4,6 @@
 LIBNAME_A=libgui-common.a
 
 
-#SRCCPP:=GUICentralPlaceholder.cpp \
-	GUICommonGui.cpp \
-	GUIPushButton.cpp \
-	GUIInitStyles.cpp \
-	GUILoggerBase.cpp \
-	GUICentralPlaceholder_moc.cpp \
-	GUICommonGui_moc.cpp  \
-	GUIPushButton_moc.cpp \
-	GUICringPushButton.cpp \
-	GUICringPushButton_moc.cpp \
-	GUIPainterClass.cpp \
-	GUIPainterClass_moc.cpp \
-	GUIGraphicalIndicator.cpp \
-	GUIGraphicalSlider.cpp
-
-
 SRCCPP:=GUICommonGui.cpp \
 	GUIPushButton.cpp \
 	GUIInitStyles.cpp \
--- gui/logger/examples/gui-loggergui-example1/gui-loggergui-example1.mk
+++ gui/logger/examples/gui-loggergui-example1/gui-loggergui-example1.mk
@@ -1,17 +1,12 @@
 PROGRAM:=gui-loggergui-example1
 
-SRCCPP+=gui-loggergui-example1.cpp \
-	GVersion.cpp
+SRCCPP+=gui-loggergui-example1.cpp
 
-	
 export CPPFLAGS:=$(CPPFLAGS_RELAXED)
 INCLUDES+= -isystem $(QT_INCLUDE)
 
 LIBS+=  -lcmdline  -llogmaster -lutilities -lreadline \
- -lreadline -lboost_system  -lboost_filesystem -lpthread -lgui-logger  -lgui-logmaster -lapi-logmaster -lgui-common -llogmaster -lexception -lutilities -lsqlite-embc -ldl
-
-
-
+ -lreadline  -lpthread -lgui-logger  -lgui-logmaster -lapi-logmaster -lgui-common -llogmaster -lexception -lutilities -lsqlite-embc -ldl
 
 
 #LIBS+=  -lcmdline  -llogmaster -lutilities -lreadline \
