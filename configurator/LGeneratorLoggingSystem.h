c73457da297de8b6e7094cbc4e2821eecfbbc243
perthi <pth@embc.no>
2023-05-17 14:53:22 +0200

    unit tests

--- configurator/LGeneratorLoggingSystem.h
+++ configurator/LGeneratorLoggingSystem.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "LGenerator.h"
 
+
+
 namespace CONFIGURATOR
 {
     class LGeneratorLoggingSystem : public LGenerator
     {
     public:
+        LGeneratorLoggingSystem (const string fname, const LXMLInfo xmlinfo);
         virtual void GenerateContent(logentity_vec levels, sysentity_vec  systems) override;
     };
 
