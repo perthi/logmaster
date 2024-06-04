8db56aafe0656ad35bb80813de1802248dfc3ac9
perthi <per.thomas.hille@eco-stor.no>
2024-06-04 21:34:25 +0200

    cleaning up directory

--- gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/perthi/work/logmaster/gtest/googletest-1.13.0
-# Build directory: /home/perthi/work/logmaster/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/perthi/work/logmaster/gtest/googletest-1.13.0/googletest
-# Build directory: /home/perthi/work/logmaster/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
