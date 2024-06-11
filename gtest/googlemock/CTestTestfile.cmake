ec8123a7e7f4f767ce6897fe0f2b3a03f177e950
perthi <per.thomas.hille@eco-stor.no>
2024-06-11 18:59:07 +0200

    1354

--- gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/perthi/work/EcoPlatform/ems/ems_cpp/submodules/logmaster/gtest/googletest-1.13.0/googlemock
-# Build directory: /home/perthi/work/EcoPlatform/ems/ems_cpp/submodules/logmaster/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
