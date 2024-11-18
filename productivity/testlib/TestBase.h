8b1c2267eb87c793f25ebc3c681428ec65827950
perthi <per.thomas.hille@eco-stor.no>
2024-11-18 08:57:49 +0100

    reduced time delay when tearing down environment

--- productivity/testlib/TestBase.h
+++ productivity/testlib/TestBase.h
@@ -65,7 +65,7 @@ class Environment : public ::testing::Environment {
   // Override this to define how to tear down the environment.
   void TearDown() override 
   {
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	//POP();
   }
 };
