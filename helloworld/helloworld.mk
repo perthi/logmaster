e5ebb56acda4c8e32360d6c425698d6be7f8d422
Per Thomas Hille <pth@embc.no>
2025-08-28 17:03:41 +0200

    removing uncessary arm linking since apllication will only be compiled for x86

--- helloworld/helloworld.mk
+++ helloworld/helloworld.mk
@@ -3,11 +3,6 @@ PROGRAM:=helloworld
 SRCCPP+=helloworld.cpp \
         GVersion.cpp
 
-
 LIBS+=    -lcmdline -llogmaster  -lutilities  -llogmaster  -lutilities  -lconfigurator -lsqlite-embc -lxml-embc  -lxml2 -lreadline -lhistory -lncurses -ldl -lpthread
-
-ifneq (arm, $(TARGET))
-LIBS+=   -lreadline 
-endif
 
 include ../../common.mk
