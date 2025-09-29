bb850af3e5eba9dfc09f05650ad4cd74e14dcb33
Per Thomas Hille <pth@embc.no>
2025-09-29 17:30:56 +0200

    LOG-34 adding the possibility to pass files to check as comman line arguments

--- cppcheck.sh
+++ cppcheck.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-cppcheck logging productivity/utilitie -I include --suppress=missingIncludeSystem  --check-level=exhaustive  --report-type=misra-cpp-2023  --platform=unix64  --language=c++ --enable=all 2>  report.txt
+if [ "$#" -eq 0 ]; then
+  echo "Error: At least one parameter is required." >&2
+  exit 1
+fi
+
+cppcheck $1 $2 $3 $4 $5 -I include --suppress=missingIncludeSystem  --check-level=exhaustive  --report-type=misra-cpp-2023  --platform=unix64  --language=c++ --enable=all 2>  report.txt
