5da2256eb0ca0c60d0b94d226864a18d30e4574c
Per Thomas Hille <pth@embc.no>
2025-10-01 08:52:35 +0200

    more descriptive name both on script and output

---
+++ misra-cppcheck.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ "$#" -eq 0 ]; then
+  echo "Error: At least one parameter is required." >&2
+  exit 1
+fi
+
+cppcheck $1 $2 $3 $4 $5 -I include --suppress=missingIncludeSystem  --suppress=unusedFunction --check-level=exhaustive  --report-type=misra-cpp-2023  --platform=unix64  --language=c++ --enable=all 2>  misra-report.txt
