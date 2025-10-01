179b3a940376402df3817e98f8ccbf08c040175e
Per Thomas Hille <pth@embc.no>
2025-10-01 08:52:57 +0200

    refactoring

--- run-coverage.sh
+++ run-coverage.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-files=`find . -name "*.gcno*"`
 
-echo "FILES=" $files
-
-for file in $files; do
-   # echo FILE = $file
-    mv $file coverage
-done
-
-files=`find . -name "*.gcda*"`
+files=`find . -name "*.gcno*" -o -name "*.gcda*"`
 
 for file in $files; do
-   # echo FILE = $file
-    mv $file coverage
+  #  echo FILE = $file
+    cp $file coverage
 done
 
-# lcov --capture --directory coverage --output-file coverage.info 
+#exit
+
+
 lcov  --capture   --directory coverage --output-file  coverage.info  --ignore-errors inconsistent
-lcov --remove coverage.info '/usr/*' 'tests/*' --output-file coverage.cleaned.info  --ignore-errors inconsistent
-#lcov --remove coverage.info '/usr/*' 'tests/*' --output-file coverage.cleaned.info  --ignore-errors inconsistent
+lcov --remove coverage.info '/usr/*' 'tests/*' 'Test*' '*3rd-party*' '*logmaster/json*' --output-file coverage.cleaned.info  --ignore-errors inconsistent
 genhtml coverage.cleaned.info --output-directory coverage-report
+rm  coverage.info overage.cleaned.info
 
 firefox coverage-report/index.html
